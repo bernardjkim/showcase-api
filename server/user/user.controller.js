7fe466d977063254ec59a8acba9e7ac7c488f694
Bernard Kim <bernardkim.seattle@gmail.com>
2019-03-04 14:01:12 -0800

    cleanup user controller

--- server/user/user.controller.js
+++ server/user/user.controller.js
@@ -1,5 +1,7 @@
 const httpStatus = require('http-status');
 const mqClient = require('../../system/amqp');
+const { checkError, docToMsg, msgToDoc } = require('../../util/mq');
+const EXCHANGE = 'api';
 
 /**
  * Load user and append to req
@@ -7,16 +9,13 @@ const mqClient = require('../../system/amqp');
 async function load(req, res, next, id) {
   const query = { _id: id };
   mqClient
-    .publish(Buffer.from(JSON.stringify(query)), 'api', 'db.req.user.get')
-    .then(reply => {
-      const doc = JSON.parse(reply.toString());
-      if (doc.error)
-        return next(new APIError(doc.error.message), httpStatus.BAD_REQUEST);
-      else req.user = doc.user;
-      next();
-    })
+    .publish(docToMsg(query), EXCHANGE, 'db.req.user.get')
+    .then(msgToDoc)
+    .then(checkError)
+    .then(doc => (req.user = doc.user))
     .catch(next);
 }
+
 /**
  * Get user
  * @property  {User}  req.user  - User object
@@ -41,33 +40,31 @@ async function create(req, res, next) {
   };
 
   mqClient
-    .publish(Buffer, from(JSON.stringify(user)), 'api', 'db.req.user.create')
-    .then(reply => {
-      const doc = JSON.parse(reply.toString());
-      if (doc.error)
-        return next(new APIError(doc.error.message), httpStatus.BAD_REQUEST);
-      res.status(httpStatus.CREATED).json({ user: doc.user });
-    });
+    .publish(docToMsg(user), EXCHANGE, 'db.req.user.create')
+    .then(msgToDoc)
+    .then(checkError)
+    .then(doc => res.status(httpStatus.CREATED).json({ user: doc.user }))
+    .catch(next);
 }
 
 async function update(req, res, next) {
   const query = {};
   const user = {};
   mqClient
-    .publish(
-      Buffer.from(JSON.stringify({ query, user })),
-      'api',
-      'db.req.user.update',
-    )
-    .then(doc => res.json({ user: JSON.parse(doc.toString()) }))
+    .publish(docToMsg({ query, user }), EXCHANGE, 'db.req.user.update')
+    .then(msgToDoc)
+    .then(checkError)
+    .then(doc => res.json({ user: doc.user }))
     .catch(next);
 }
 
 async function remove(req, res, next) {
   const query = {};
   mqClient
-    .publish(Buffer.from(JSON.stringify(query)), 'api', 'db.req.user.delete')
-    .then(doc => res.json({ user: JSON.parse(doc.toString()) }))
+    .publish(docToMsg(query), EXCHANGE, 'db.req.user.delete')
+    .then(msgToDoc)
+    .then(checkError)
+    .then(doc => res.json({ user: doc.user }))
     .catch(next);
 }
 
