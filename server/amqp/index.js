16f0a38738cb4b12b1aeaf76f4a4a65e8ab7b15c
Bernard Kim <bernardkim.seattle@gmail.com>
2019-03-11 15:22:15 -0700

    env

--- server/amqp/index.js
+++ server/amqp/index.js
@@ -1,6 +1,8 @@
 const amqp = require('../../system/amqp');
+const config = require('../../config/config');
 
-const mqClient = new amqp.Connection();
+const { host, port } = config.rabbit;
+const mqClient = new amqp.Connection(`amqp://${host}:${port}`);
 const exchange = mqClient.declareExchange('api', 'topic');
 
 module.exports = { mqClient, exchange };
