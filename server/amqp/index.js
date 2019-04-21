7bc41fa38d5cf95d43fc76578a7271e9869bb77f
Bernard Kim <bernardkim.seattle@gmail.com>
2019-04-20 21:40:08 -0700

    deleted unecessary files

--- server/amqp/index.js
+++
@@ -1,8 +0,0 @@
-const amqp = require('@bernardjkim/amqplib');
-const config = require('../../config/config');
-
-const { host, port } = config.rabbit;
-const mqClient = new amqp.Connection(`amqp://${host}:${port}`);
-const exchange = mqClient.declareExchange('api', 'topic');
-
-module.exports = { mqClient, exchange };
